d18a02e55b16c37a782713a6413cff591e3856f8
agustinPassetti <agustinpassetti@gmail.com>
2020-10-13 16:17:17 -0300

    commiting math folder

---
+++ Practice/python_handon/done/math/flip_a_coin.py
@@ -0,0 +1,18 @@
+# Write a Python program to flip a coin 1000 times and count heads and tails.
+
+"""import random
+import itertools
+results = {
+    'heads': 0,
+    'tails': 0,
+}
+sides = list(results.keys())
+for i in range(10):
+    
+    results[(random.choice(sides))] += 1
+#print('Heads:', results['heads'])
+#print('Tails:', results['tails'])
+"""
+
+#no funca, no anda el choice en random
+# lo probé en una virtual machine de w3school y anduvo bien
---
+++ Practice/python_handon/done/math/random.py
@@ -0,0 +1,13 @@
+#  Write a Python program to generate a series of unique random numbers"
+
+"""import random
+
+choices = list(range(100))
+random.shuffle(choices)
+print(choices.pop())
+while choices:
+    print(choices.pop())"""
+
+
+#no funca, no anda el shuffle en random
+# lo probé en una virtual machine de w3school y anduvo bien
